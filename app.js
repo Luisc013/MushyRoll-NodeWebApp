083fe18a694fe096d1557139449d5edc9aec75c4
Luisc013 <c.luis013@gmail.com>
2021-01-27 11:46:15 -0500

    Coded a way to serve static images/files from public dir, added favicon for site title and brand img for navbar

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 var express = require("express");
 var path = require("path");
 
-// var routes = require("./routes");
-
 var app = express();
 
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
+
+// serve static files 
+app.use(express.static(path.join(__dirname, "public")));
+
+//set view engine
 app.set("view engine", "ejs");
 
+// routes
 app.use("/", require("./routes/web"));
 app.use("/api", require("./routes/api"));
 
